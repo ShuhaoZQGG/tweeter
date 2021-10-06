0dcfa08fa0fe243cc112e8a63c6df7ddfc74bcb6
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-06 14:06:07 -0400

    try to implement create a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweet_post =  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1633313862833
+};
+
+const $tweet = $(`<article class="tweets-article"></article>`);
+console.log($tweet);
+$('#tweets-container').append($tweet);
+
