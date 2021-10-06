d7a93555c2241db93e0880acda1065816337f912
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-06 15:42:04 -0400

    add newtweet to the container whenver hit the submit button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweet_post =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+$(document).ready(function() {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1633313862833
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(tweet_Data) {
  const tweet_article = `
@@ -40,8 +54,45 @@ const createTweetElement = function(tweet_Data) {
 
  return tweet_article;
 };
-$(document).ready(function() {
-  const $tweet = createTweetElement(tweet_post);
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+
+const renderTweets = function(tweets_data) {
+  $('#tweets-container').empty();
+
+  for (const tweet of tweets_data) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
+  }
+  return $('#tweets-container');
+}
+
+  renderTweets(data);
+
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: "GET",
+      dataType: "json",
+      success: (tweets) => {
+        console.log(tweets);
+        renderTweets(tweets);
+      },
+
+      error: (err) => {
+        console.log(`there was an error ${err}`)
+      }
+    });
+  }
+
+  const $form = $("#newTweet-form");
+
+  $form.on("submit", function(event){
+  event.preventDefault();
+
+  const serializeData = $(this).serialize();
+  
+  $.post("/tweets", serializeData, (res) =>{
+    loadTweets();
+  })
+  })  
 });
+
