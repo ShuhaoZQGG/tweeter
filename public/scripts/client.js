6abeb74afd713cc9abb666862fe7a90afc5a94b2
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-06 14:31:30 -0400

    finish createNewTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,7 +16,32 @@ const tweet_post =  {
   "created_at": 1633313862833
 };
 
-const $tweet = $(`<article class="tweets-article"></article>`);
-console.log($tweet);
-$('#tweets-container').append($tweet);
+const createTweetElement = function(tweet_Data) {
+ const tweet_article = `
+<article class = "tweets-article">
+  <header>
+    <div class = "avatar-name">
+    <img src = ${tweet_Data.user.avatars}>
+    <h6>${tweet_Data.user.name}</h6>
+    </div>
+    <h5>${tweet_Data.user.handle}</h5>
+  </header>
+  <p>${tweet_Data.content.text}</p>
+  <footer class = "tweets-footer">
+    <p id = "tweet-time">${tweet_Data.created_at}</p>
+    <div class = "interaction-buttons">
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-comment"></i>
+      <i class="fa fa-retweet" aria-hidden="true"></i>
+    </div>
+  </footer>
+</article>
+ `;
 
+ return tweet_article;
+};
+$(document).ready(function() {
+  const $tweet = createTweetElement(tweet_post);
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
+});
