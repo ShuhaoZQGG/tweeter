9f9b5ba8a77b207f0fdb6c712e3d40f473fc380b
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-06 23:24:10 -0400

    add a scroll up button whenever use scroll down and then hide the nav

--- public/scripts/back_to_new_tweet.js
+++ public/scripts/back_to_new_tweet.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   let count = 0;
+
+  
+
   $(".newTweet").on("click", function(){
     count ++;
 
@@ -8,12 +11,35 @@ $(document).ready(function(){
         scrollTop: $("#tweet-text").offset().top - 190
         }, 1000);
       $('#tweet-text').focus();
+      $("#up-button").css("display", "none");
     } else {
       $('html, body').animate({
         scrollTop: $(".tweets-container").offset().top - 110
         }, 1000);
     }
-
   })
 
+  $(".newTweet").on("mouseover hover",function(){
+    $(".fa-angle-double-down").animate({
+      top: "40px"
+    });
+    $(".fa-angle-double-down").animate({
+      top: "30px"
+    });
+  })
+
+  $(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta < 0) {
+      $("#up-button").css("display", "inline-flex");
+      $("nav").css("visibility", "hidden");
+    }
+});
+
+  $("#up-button").on("click", function(){
+    $('html, body').animate({
+      scrollTop: $(".header").offset().top - 190
+      }, 1000);
+    $(this).css("display", "none");
+    $("nav").css("visibility", "visible");
+  })
 })
